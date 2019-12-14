983984425448fd6e7e1188a6bb7a384445130375
uni51 <uni51@users.noreply.github.com>
2019-12-14 11:17:21 +0900

    install & setup Laravel ユーザー登録&ログインまで確認

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,23 @@
+<!-- resources/views/auth/login.blade.php -->
+
+<form method="POST" action="/auth/login">
+    {!! csrf_field() !!}
+
+    <div>
+        メールアドレス
+        <input type="email" name="email" value="{{ old('email') }}">
+    </div>
+
+    <div>
+        パスワード
+        <input type="password" name="password" id="password">
+    </div>
+
+    <div>
+        <input type="checkbox" name="remember"> ログインを継続する
+    </div>
+
+    <div>
+        <button type="submit">ログイン</button>
+    </div>
+</form>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,30 @@
+<!-- resources/views/auth/register.blade.php -->
+
+<form method="POST" action="/auth/register">
+    {!! csrf_field() !!}
+
+    <div>
+        ユーザー名
+        <input type="text" name="name" value="{{ old('name') }}">
+    </div>
+
+    <div>
+        メールアドレス
+        <input type="email" name="email" value="{{ old('email') }}">
+    </div>
+
+    <div>
+        パスワード
+        <input type="password" name="password">
+    </div>
+
+    <div>
+        パスワード確認
+        <input type="password" name="password_confirmation">
+    </div>
+
+    <div>
+        <button type="submit">登録</button>
+    </div>
+</form>
+認証
