7e8498533982175893f4e9ccfe7229c9163395e4
uni51 <uni51@users.noreply.github.com>
2019-12-18 16:48:59 +0900

    finish 6-1

--- app/Repositories/BookRepository.php
+++ app/Repositories/BookRepository.php
@@ -30,36 +30,36 @@ class BookRepository
      * @throws \Exception
      * @return null|array
      */
-//    public function getReferenceBook($id = null)
-//    {
-//        if (is_null($id)) {
-//            throw new \Exception;
-//        }
-//        foreach ($this->books as $book) {
-//            if ($book['id'] === $id) {
-//                return $book;
-//            }
-//        }
-//        return null;
-//    }
+    public function getReferenceBook($id = null)
+    {
+        if (is_null($id)) {
+            throw new \Exception;
+        }
+        foreach ($this->books as $book) {
+            if ($book['id'] === $id) {
+                return $book;
+            }
+        }
+        return null;
+    }
 
     /**
      * @param array $params
      * @throws \Exception
      */
-//    public function setReferenceBook(array $params)
-//    {
-//        if (!isset($params['id'], $params['title'])) {
-//            throw new \Exception;
-//        }
-//        $this->books[] = $params;
-//    }
+    public function setReferenceBook(array $params)
+    {
+        if (!isset($params['id'], $params['title'])) {
+            throw new \Exception;
+        }
+        $this->books[] = $params;
+    }
 
     /**
      * @return string
      */
-//    protected function getText()
-//    {
-//        return 'Laravel5';
-//    }
+    protected function getText()
+    {
+        return 'Laravel5';
+    }
 }
--- tests/Repositories/BookRepositoryTest.php
+++ tests/Repositories/BookRepositoryTest.php
@@ -42,111 +42,112 @@ class BookRepositoryTest extends \PHPUnit_Framework_TestCase
     /**
      * リスト6.5：配列内に特定のキーをテスト
      */
-//    public function testReturnResult()
-//    {
-//        $this->assertInternalType('array', $this->repository->getReferenceBooks());
-//        foreach ($this->repository->getReferenceBooks() as $book) {
-//            $this->assertArrayHasKey('title', $book);
-//        }
-//    }
+    public function testReturnResult()
+    {
+        $this->assertInternalType('array', $this->repository->getReferenceBooks());
+        foreach ($this->repository->getReferenceBooks() as $book) {
+            $this->assertArrayHasKey('title', $book);
+        }
+    }
 
     /**
      * リスト6.7：getReferenceBook をテストするコード
      * @throws \Exception
      */
-//    public function testReturnBook()
-//    {
-//        $result = $this->repository->getReferenceBook(1);
-//        $this->assertInternalType('array', $result);
-//        $this->assertArrayHasKey('title', $result);
-//        $this->assertArrayHasKey('id', $result);
-//        $this->assertNull($this->repository->getReferenceBook(10));
-//    }
+    public function testReturnBook()
+    {
+        $result = $this->repository->getReferenceBook(1);
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('title', $result);
+        $this->assertArrayHasKey('id', $result);
+        $this->assertNull($this->repository->getReferenceBook(10));
+    }
 
     /**
      * リスト6.8：例外をテスト
      * @expectedException \Exception
      */
-//    public function testReturnBookException()
-//    {
-//        $this->repository->getReferenceBook();
-//    }
+    public function testReturnBookException()
+    {
+        $this->repository->getReferenceBook();
+    }
 
     /**
      * @expectedException \Exception
      */
-//    public function testAddBookException()
-//    {
-//        $this->repository->setReferenceBook([]);
-//        $this->repository->setReferenceBook(['id' => 1]);
-//        $this->repository->setReferenceBook(['title' => 'testing']);
-//    }
+    public function testAddBookException()
+    {
+        $this->repository->setReferenceBook([]);
+        $this->repository->setReferenceBook(['id' => 1]);
+        $this->repository->setReferenceBook(['title' => 'testing']);
+    }
 
     /**
      * リスト6.10：データプロバイダを利用したテストコード
      * @dataProvider addBookData
      */
-//    public function testSetBooks(array $params)
-//    {
-//        $this->repository->setReferenceBook($params);
-//        $books = $this->repository->getReferenceBooks();
-//        foreach ($books as $book) {
-//            $this->assertArrayHasKey('title', $book);
-//            $this->assertArrayHasKey('id', $book);
-//        }
-//    }
+    public function testSetBooks(array $params)
+    {
+        $this->repository->setReferenceBook($params);
+        $books = $this->repository->getReferenceBooks();
+        foreach ($books as $book) {
+            $this->assertArrayHasKey('title', $book);
+            $this->assertArrayHasKey('id', $book);
+        }
+    }
 
     /**
      * 書籍データを追加
      * @return array
      */
-//    public function addBookData()
-//    {
-//        return [
-//            [
-//                [
-//                    'id' => 2,
-//                    'title' => 'AngularJSリファレンス'
-//                ],
-//            ]
-//        ];
-//    }
+    public function addBookData()
+    {
+        return [
+            [
+                [
+                    'id' => 2,
+                    'title' => 'AngularJSリファレンス'
+                ],
+            ]
+        ];
+    }
 
     /**
      * リスト6.11：テスト間の依存
      * @return array|null
      * @throws Exception
      */
-//    public function testReturnBookDepend()
-//    {
-//        $result = $this->repository->getReferenceBook(1);
-//        $this->assertInternalType('array', $result);
-//        $this->assertArrayHasKey('title', $result);
-//        $this->assertArrayHasKey('id', $result);
-//        $this->assertNull($this->repository->getReferenceBook(10));
-//        return $result;
-//    }
+    public function testReturnBookDepend()
+    {
+        $result = $this->repository->getReferenceBook(1);
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('title', $result);
+        $this->assertArrayHasKey('id', $result);
+        $this->assertNull($this->repository->getReferenceBook(10));
+        return $result;
+    }
 
     /**
      * @depends testReturnBookDepend
      * @test
      */
-//    public function 依存を利用したテスト($params)
-//    {
-//        $this->assertSame($params['title'], 'Laravelリファレンス');
-//    }
+    public function 依存を利用したテスト($params)
+    {
+        $this->assertSame($params['title'], 'Laravelリファレンス');
+    }
 
     /**
      * リスト6.13：protected 宣言されたメソッドをテスト 1
      * @test
      */
-//    public function protectedメソッドに対してリフレクションを利用したテスト()
-//    {
-//        $reflectionClass = new \ReflectionClass($this->repository);
-//        $reflectionMethod = $reflectionClass->getMethod('getText');
-//        $reflectionMethod->setAccessible(true);
-//        $this->assertEquals('Laravel5', $reflectionMethod->invoke($this->repository));
-//    }
+    public function protectedメソッドに対してリフレクションを利用したテスト()
+    {
+        $reflectionClass = new \ReflectionClass($this->repository);
+        $reflectionMethod = $reflectionClass->getMethod('getText');
+        $reflectionMethod->setAccessible(true);
+        // ReflectionMethod::invoke — 実行する
+        $this->assertEquals('Laravel5', $reflectionMethod->invoke($this->repository));
+    }
 
     /**
      * リスト6.13：protected 宣言されたメソッドをテスト 2
